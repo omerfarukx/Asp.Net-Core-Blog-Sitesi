f4871ff299d6c2b62ce6ee2bd259a0f51c0361a9
Omer Faruk <omerfarukiskk@gamil.com>
2022-12-02 22:45:13 +0300

    Yazar blog ekleme ve silme yapıldı

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CoreDemo.Controllers
 {
@@ -17,8 +21,55 @@ namespace CoreDemo.Controllers
         public IActionResult BlogReadAll(int id)
         {
             ViewBag.i = id;
-            var values  = bm.GetBlogByID(id);
+            var values = bm.GetBlogByID(id);
             return View(values);
+        }
+        public IActionResult BlogListByWriter()
+        {
+            var values = bm.GetListWithCategoryByWriterBm(1);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> categoryvalues = (from x in cm.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }
+                                                   ).ToList();
+            ViewBag.cv = categoryvalues;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator wv = new BlogValidator();
+            ValidationResult result = wv.Validate(p);
+            if (result.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteBlog(int id)
+        {
+            var blogvalue = bm.TGetById(id);
+            bm.TDelete(blogvalue);
+            return RedirectToAction("BlogListByWriter");
         }
     }
 }
--- DataAccesLayer/Abstract/IBlogDal.cs
+++ DataAccesLayer/Abstract/IBlogDal.cs
@@ -10,5 +10,6 @@ namespace DataAccesLayer.Abstract
     public interface IBlogDal:IGenericDal<Blog>
     {
         List<Blog> GetListWithCategory();
+        List<Blog> GetListWithCategoryByWriter(int id);
     }
 }
