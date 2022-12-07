0982723b3e618b34f9d67906527a89549dc16865
Omer Faruk <omerfarukiskk@gamil.com>
2022-12-07 21:59:26 +0300

    Areas oluşturuldu

---
+++ CoreDemo/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
