17548751ddefa24e8611f7e9e0b4ef918cad4cd8
Omer Faruk <omerfarukiskk@gamil.com>
2022-12-11 23:07:57 +0300

    web api üzerinde denemeler yapıldı

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
